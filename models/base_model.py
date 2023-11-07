d3e8ce3923e796d145f51c1952fac2481b561b45
yandah1 <nonku003@gmail.com>
2023-11-07 21:39:55 +0200

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,38 @@
 #!/usr/bin/python3
 """BaseModel that defines all common attributes/methods
 for other classes:"""
-
 import uuid
 from datetime import datetime
-import  models
 
 class BaseModel:
     """Defining a class base model"""
 
-    def __init__(self):
-        """Initialized attribute of base class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialization of base class instance
+
+        Args:
+            *args: arguments
+            **kwargs: key-value arguments
+        """
+        if kwargs:
+            kwargs.pop("__class__", None)
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+        self.storage = None
+        self.storage_new()
+
+    def storage_new(self):
+        """Method to store the object using the storage mechanism"""
+        if self.storage is not None:
+            self.storage.new(self)
 
     def __str__(self):
         """Defining str method"""
@@ -22,6 +41,7 @@ class BaseModel:
     def save(self):
         """Defining save method"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Defining to_dict method"""
