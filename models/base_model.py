a4d9f4e61b3bd7f455f39a1c6fad8932a6204e62
yandah1 <nonku003@gmail.com>
2023-11-07 13:41:51 +0200

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
-
-"""base class that defines all common attributes/methods for other classes"""
+"""BaseModel that defines all common attributes/methods
+for other classes:"""
 
 import uuid
-import datetime
+from datetime import datetime
+import  models
 
-class BaseModel():
+class BaseModel:
+    """Defining a class base model"""
 
-"""defining a class base model"""
+    def __init__(self):
+        """Initialized attribute of base class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-def __init__ (self):
-    self.id = str(uuid4())
-    self.created_at = datetime 
+    def __str__(self):
+        """Defining str method"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Defining save method"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Defining to_dict method"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
