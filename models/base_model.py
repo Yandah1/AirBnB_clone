17790db43d1b7e1c1df26c14dd1bbc2d632319fe
yandah1 <nonku003@gmail.com>
2023-11-08 21:28:34 +0200

    test

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
     """Defining a class base model"""
 
@@ -25,20 +26,13 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
-
-        self.storage = None
-        self.storage_new()
-
-    def storage_new(self):
-        """Method to store the object using the storage mechanism"""
-        if storage is not None:
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
         """Defining str method"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__,
+                self.id, self.__dict__)
 
     def save(self):
         """Defining save method"""
