5012398ae5a6e13832b214686dc0e07bb910ac73
yandah1 <nonku003@gmail.com>
2023-11-09 14:09:31 +0200

    console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,34 +7,34 @@ import models
 
 class FileStorage:
     """Class for persistent storage"""
-    _file_path = "file.json"
-    _objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """Returns _objects dictionary"""
-        return self._objects
+        return self.__objects
 
     def new(self, obj):
         """Sets new objects"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        self._objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Saves all files"""
         objs = {}
-        for k, v in self._objects.items():
+        for k, v in self.__objects.items():
             objs[k] = v.to_dict()
 
-        with open(self._file_path, 'w') as file:
+        with open(self.__file_path, 'w') as file:
             json.dump(objs, file)
 
     def reload(self):
         """Deserializes the JSON file to _objects"""
-        if not os.path.isfile(self._file_path):
+        if not os.path.isfile(self.__file_path):
             return
 
-        with open(self._file_path, 'r') as file:
+        with open(self.__file_path, 'r') as file:
             obj_dict = json.load(file)
             obj_dict = {k: self.classes()[v['__class__']](**v)
                         for k, v in obj_dict.items()}
-            self._objects = obj_dict
+            self.__objects = obj_dict
