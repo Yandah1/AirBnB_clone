0d53fe284c2f2ea27ca32dccfc9f0b5a89031d04
yandah1 <nonku003@gmail.com>
2023-11-09 14:25:22 +0200

    console

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,6 @@
 from .engine.file_storage import FileStorage
 
 storage = FileStorage()
+storage.reload()
 #storage._Filestorage__file_path = 'data.json'
 #storage._FileStorage__objects = {} models = {}
