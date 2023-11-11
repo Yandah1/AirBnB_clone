dde0e6bc16d3c6cfad3fa052a4d4d03ca518f88a
yandah1 <nonku003@gmail.com>
2023-11-11 18:09:42 +0200

    f

--- console.py
+++ console.py
@@ -32,7 +32,6 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program
         """
-                                      
         exit(0)
 
     def do_EOF(self, arg):
